ce86363a321d2e69a491ab8af63bbc100e813cd6
abdullah-shah <abi.salim5@gmail.com>
2023-07-12 19:25:16 +0500

    Home.vue: Added Toggle content button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,29 @@
 <script setup>
 import {ref} from 'vue';
-const msg = ref('Home')
+const msg = ref(' From Toggle Content')
+const isOpen = ref(true);
 </script>
 <template >
-    <h1>Hello from {{ msg }}</h1>
+    <div class="container">
+    <h1 v-if="isOpen">Hello {{ msg }}</h1>
+    <!-- <h2 v-else>Hello Vue.</h2> -->
+    <button @click="isOpen = !isOpen">{{isOpen? 'Hide Content': 'Show Content'}}</button>
+    
+</div>
 </template>
 <style scoped>
 h1{
     text-align:center;
+}
+.container {
+    position: relative;
+    height:50vh
+}
+button{
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+
 }
 </style>
